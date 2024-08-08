f1d2c7607fbbe9338cb9012cc1ad9bf4f26374ae
Enise <eniseaslan2001@gmail.com>
2024-08-08 14:50:27 +0300

    component oluşturuldu hatalar giderildi

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { AddMinus } from "./components/addMinus";
 
-import { useState, useEffect } from "react";
-import ButtonQuantity from "./components/buttonQuantity";
-
 function App() {
-  const [sayi, setCount] = useState(0);
-  const Up = () => {
-    setCount((up) => up + 1);
-  };
-  const Down = () => {
-    setCount((down) => down - 1);
-  };
-
-
-
-
   return (
-    <>
-      <h1>
-        <div> Sayı: {sayi}</div>
-      </h1>
-      <body>
-<div class= "general">
-      <ButtonQuantity onClick={Down} innerText={"-"} className="left-btn"  disabled={sayi === 0}/>
-      <text class="text"> {sayi} </text>
-      <ButtonQuantity onClick={Up} innerText={"+"} className="right-btn"/>
-      
-      </div>
-      </body>
-    </>
+    <div className="container">
+      <AddMinus title="Quantity" />
+      <AddMinus title="Money" />
+    </div>
   );
 }
 
--- src/components/addMinus.jsx
+++ src/components/addMinus.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import ButtonQuantity from "./buttonQuantity";
 
-export const AddMinus = () => {
+export const AddMinus = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const Up = () => {
+    setCount((up) => up + 1);
+  };
+  const Down = () => {
+    setCount((down) => down - 1);
+  };
+
   return (
-    <div>addMinusdeneme</div>
-  )
-}
+    <>
+      <div>
+        <h1>
+          {" "}
+          {title}:{count}
+        </h1>
+      </div>
+
+      <div class="card">
+        <ButtonQuantity
+          onClick={Down}
+          innerText={"-"}
+          className="left-btn"
+          disabled={count === 0}
+        />
+        <h6 class="text"> {count} </h6>
+        <ButtonQuantity onClick={Up} innerText={"+"} className="right-btn" />
+      </div>
+    </>
+  );
+};
